0c3ee5ce97c35e25a3d0ce23dab10fc9f3b0fec4
callmeaf <alikhoshkar2000@gmail.com>
2024-11-22 08:41:54 +0330

    Add comment in routes

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -3,6 +3,7 @@
 use \Illuminate\Support\Facades\Route;
 
 Route::prefix(config('callmeaf-base.api.prefix_url'))->as(config('callmeaf-base.api.prefix_route_name'))->middleware(config('callmeaf-base.api.middlewares'))->group(function() {
+    // Carts
     Route::apiResource('carts',config('callmeaf-cart.controllers.carts'));
     Route::prefix('carts')->as('carts.')->controller(config('callmeaf-cart.controllers.carts'))->group(function() {
         Route::prefix('{cart}')->group(function() {
@@ -14,10 +15,7 @@ Route::prefix(config('callmeaf-base.api.prefix_url'))->as(config('callmeaf-base.
         });
         Route::get('/trashed/index','trashed')->name('trashed.index');
     });
-});
-
-
-Route::prefix(config('callmeaf-base.api.prefix_url'))->as(config('callmeaf-base.api.prefix_route_name'))->middleware(config('callmeaf-base.api.middlewares'))->group(function() {
+    // Cart Items
     Route::apiResource('cart_items',config('callmeaf-cart-items.controllers.cart_items'));
     Route::prefix('cart_items')->as('cart_items.')->controller(config('callmeaf-cart-items.controllers.cart_items'))->group(function() {
         Route::post('future/add',[config('callmeaf-cart-items.controllers.cart_items'),'storeInFuture']);
